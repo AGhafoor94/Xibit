c40aa7d30b0cb9e66f0819868c83a76552b11359
Adnan <adnan_ghafoor94@hotmail.com>
2020-08-24 18:23:02 +0100

    Using ant design instead of material ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "antd/dist/antd.css";
 import "./App.css";
+import { Row, Col } from "antd";
 
-function App() {
-  return <div></div>;
-}
-
+const App = () => {
+  return (
+    <div>
+      <Row>
+        <Col className="firstColumn" span={12}>
+          col-12
+        </Col>
+        <Col className="secondColumn" span={12}>
+          col-12
+        </Col>
+      </Row>
+    </div>
+  );
+};
 export default App;
