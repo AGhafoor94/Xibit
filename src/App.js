6d7b408af6c8e5f06cbf36634aeeefe05e27c0a0
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-14 18:12:15 +0100

    created 2 columns at 100% height

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Box, Columns } from "react-bulma-components";
+import { Columns } from "react-bulma-components";
 
 const App = () => {
   return (
     <div>
-      <Box>
-        <Columns breakpoint="desktop">
-          <Columns.Column>
-            <p className="bd-notification is-success">First Column</p>
-          </Columns.Column>
-          <Columns.Column>
-            <p className="bd-notification is-info">Second Column</p>
-          </Columns.Column>
-        </Columns>
-      </Box>
+      <Columns breakpoint="desktop" style={{ height: "100vh" }}>
+        <Columns.Column style={{ backgroundColor: "orange" }}>
+          <p className="bd-notification is-success">First Column</p>
+        </Columns.Column>
+        <Columns.Column>
+          <p className="bd-notification is-info">Second Column</p>
+        </Columns.Column>
+      </Columns>
     </div>
   );
 };
