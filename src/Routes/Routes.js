dcdb5ce2517cb414e5dbb36d44198ba6cf26fe6e
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-16 20:01:25 +0100

    Working on the dashboard

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Login from "../components/Loginpage/Login";
-import Plans from "../components/Plans/Plans";
+import Plans from "../components/LoggedInComponents/Plans/Plans";
 import Register from "../components/RegisterPage/Register";
 import UserContext from "../Context/UserContext";
+import Dashboard from "../components/LoggedInComponents/Dashboard/Dashboard";
 
 const Routes = () => {
   const userContext = useContext(UserContext);
@@ -16,14 +17,17 @@ const Routes = () => {
           <Route path="/" exact>
             {user.token ? <Redirect to="/" /> : <Login />}
           </Route>
+          <Route path="/dashboard" exact>
+            {user.token ? <Dashboard /> : <Redirect to="/login" />}
+          </Route>
           <Route path="/login" exact>
             {user.token ? <Redirect to="/dashboard" /> : <Login />}
           </Route>
           <Route path="/register" exact>
             {user.token ? <Redirect to="/login" /> : <Register />}
           </Route>
-          <Route path="/plans">
-            {user.token ? <Redirect to="/login" /> : <Plans />}
+          <Route path="/plans" exact>
+            {user.token ? <Plans /> : <Redirect to="/login" />}
           </Route>
         </Switch>
       </div>
