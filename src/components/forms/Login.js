11611c24a9bc934d9b991372e955df3e9d8b9947
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-24 20:09:33 +0100

    Changed

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, forwardRef } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
-import { BASE_URL } from "../api/constants";
+import { BASE_URL } from "../../api/constants";
 
 import UserContext from "../../context/UserContext";
 
