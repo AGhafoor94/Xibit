8ba245dc6df57fa96620011c0d04fade22b92410
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-24 19:08:10 +0100

    Updates

---
+++ precache-manifest.4f43094136793bba2fc7d1489cd0d829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07c912b2985f85dc492ad17590fa485",
+    "url": "/xibit/index.html"
+  },
+  {
+    "revision": "82d4b5b371879f2a65d2",
+    "url": "/xibit/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "eaad3f3f4c52ce6d4c0d",
+    "url": "/xibit/static/css/main.fc642ba1.chunk.css"
+  },
+  {
+    "revision": "82d4b5b371879f2a65d2",
+    "url": "/xibit/static/js/2.1e827c2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/xibit/static/js/2.1e827c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaad3f3f4c52ce6d4c0d",
+    "url": "/xibit/static/js/main.32f6c02d.chunk.js"
+  },
+  {
+    "revision": "4661c0b2bb1c58c32e15",
+    "url": "/xibit/static/js/runtime-main.4ff6a54c.js"
+  }
+]);
